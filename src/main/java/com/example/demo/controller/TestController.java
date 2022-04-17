86ba3f1f25fe55d991cf6fda255fd337cf579e3b
seongjin <seongjinna@gmail.com>
2022-04-17 11:24:35 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,63 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TestRequestBodyDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+    @GetMapping
+    public String testController() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/testGetMapping")
+    public String testControllerWithPath() {
+        return "Hello World! testGetMapping";
+    }
+
+    @GetMapping("/{id}")
+    public String testControllerWithPathVariables(@PathVariable(required = false) int id) {
+        return "Hello World! ID " + id;
+    }
+
+    @GetMapping("/testRequestParam")
+    public String testControllerWithRequestParam(@RequestParam(required = false) int id) {
+        return "Hello World! ID " + id;
+    }
+
+    @GetMapping("/testRequestBody")
+    public String testControllerWithRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+        return "Hello World! ID " + testRequestBodyDTO.getId() + " Message : " + testRequestBodyDTO.getMessage();
+    }
+
+    @GetMapping("/testResponseBody")
+    public ResponseDTO<String> testControllerResponseBody() {
+        List<String> list = new ArrayList<>();
+        list.add("Hello World! I'm ResponseDTO");
+        ResponseDTO<String> response = ResponseDTO.<String>builder()
+                .data(list)
+                .build();
+        return response;
+    }
+
+    @GetMapping("/testResponseEntity")
+    public ResponseEntity<?> testControllerResponseEntity() {
+        List<String> list = new ArrayList<>();
+        list.add("Hello World! I'm ResponseEntity. And you got 400!");
+        ResponseDTO<String> response = ResponseDTO.<String>builder()
+                .data(list)
+                .build();
+        // http status 를 400(Bad Request) 으로 설정
+        return ResponseEntity.badRequest().body(response);
+
+//        // 정상 응답 처리 시
+//        return ResponseEntity.ok().body(response);
+    }
+}
